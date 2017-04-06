f765548ad29bd66d9883147567a8a21224f17c0c
yangjin <yangjin@mofangge.com>
2017-04-06 18:09:58 +0800

    提交

--- CompanyWebCore/CompanyWebCore/Program.cs
+++ CompanyWebCore/CompanyWebCore/Program.cs
@@ -11,11 +11,13 @@ namespace CompanyWebCore
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine(Bll.Class1.GetTestStr());
             var host = new WebHostBuilder()
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
+                .UseApplicationInsights()
                 .Build();
 
             host.Run();
---
+++ CompanyWebCore/Dal/Class1.cs
@@ -0,0 +1,9 @@
+﻿namespace Dal
+{
+    public class Class1
+    {
+        public static string GetTestStr() {
+            return "Test Str";
+        }
+    }
+}
